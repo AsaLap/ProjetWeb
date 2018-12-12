08ae4a71183c2cb5602b4d0accda3b6aa434149c
Charlotte André <charlotte-andre@hotmail.fr>
2018-12-12 10:12:11 +0100

    Maj:je fais ce que je peux

---
+++ main.js
@@ -0,0 +1,9 @@
+$("#donnezcarte").click(function () {
+  function getRandomIntInclusive(1, 52) {
+  1 = Math.ceil(1);
+  52 = Math.floor(52);
+  return Math.floor(Math.random() * (52 - 1 +1)) + 1;
+}
+});
+
+var img = new array("1.BMP"," 2.BMP","3.BMP","4.BMP","5.BMP","6.BMP","7.BMP","8.BMP","9.BMP","10.BMP","11.BMP","12.BMP","13.BMP","14.BMP","15.BMP","16.BMP","17.BMP","18.BMP","19.BMP","20.BMP","21.BMP","22.BMP","23.BMP"]
