99295f87578f8210eebe53d19450280d84c1c4d2
AsaLap <asalaporte@gmail.com>
2018-12-26 10:51:16 +0100

    Récupération de la taille de la fenêtre et modification du CSS en JS afin de faire du responsive design

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-function() {
-  // Sections height
-  $(window).resize(function() {
-    var sH = $(window).height();
-    $('.NomDeTaClass').css('height', sH + 'px');
-  });
+var resizeHeight = function(){
+  desk = document.getElementById("desk");
+  var sH = window.innerHeight;
+  desk.style.height = sH*80/100 + 'px';
+  desk.style.backgroundSize = sH+0.4*sH + 'px';
 }
+
+// changer l'appel pour être responsive en cas de redimensionnement de fenêtre
+window.onload = resizeHeight;
+
+//
+// $(function() {
+//   // Sections height
+//   $(window).resize(function() {
+//     var sH = $(window).height();
+//     $('.desk').css('height', sH + 'px');
+//     console.log(sH);
+//   });
+// });
