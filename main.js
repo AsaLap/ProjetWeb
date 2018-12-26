f00ebf6b1ad1465f04cd5bb2780b80b91f7ba686
AsaLap <asalaporte@gmail.com>
2018-12-26 12:05:05 +0100

    Mise en place des flexbox pour le placement des cartes du joueur et du dealer, en responsive. Le responsive est à retravailler en fonction de la largeur de l'écran (cf. commentaires sur le main.js)

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+// Modifier la fonction pour que le tout soit en fonction de la diagonale et pas seulement la hauteur (problème si page haute mais fine)
 var resizeHeight = function(){
-  desk = document.getElementById("desk");
+  desk = document.getElementById('desk');
+  cards = document.getElementById('cards');
   var sH = window.innerHeight;
   desk.style.height = sH*80/100 + 'px';
   desk.style.backgroundSize = sH+0.4*sH + 'px';
+  cards.style.paddingTop = sH/10 + 'px'; //Permet d'espacer les deux items flex dealerCards et Cards
 }
 
 // changer l'appel pour être responsive en cas de redimensionnement de fenêtre
 window.onload = resizeHeight;
 
-//
+// Fonction JQuery modification de classe (aller sur OpenClassroom pour créer une fonction de modification de classe)
 // $(function() {
 //   // Sections height
 //   $(window).resize(function() {
