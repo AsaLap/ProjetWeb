9218f8d388c3eb4f6e98527c579fba9e6147ecdb
AsaLap <asalaporte@gmail.com>
2019-01-09 13:46:31 +0100

    code propre

--- modal.js
+++ modal.js
@@ -1,7 +1,5 @@
 // Get the modal
 var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -11,23 +9,9 @@ img.onload = function(){
     captionText.innerHTML = this.alt;
     document.body.removeChild(img);
 }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-
 setTimeout(function() {
     $("#myModal").fadeOut();
 }, 1000);
-
-$("#play").click(function(){
-  console.log("coucou");
-});
-
 
 function addImgInDiv(path,id) {
   var img=document.createElement('img');
@@ -38,7 +22,6 @@ function addImgInDiv(path,id) {
 
 function modalInit(){
   var modal = document.getElementById('modalWin');
-  // Get the image and insert it inside the modal - use its "alt" text as a caption
   var modalImg = document.getElementById("img01");
   var captionText = document.getElementById("caption");
   modal.style.display = "block";
